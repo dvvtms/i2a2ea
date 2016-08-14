05e280b3af4c48991c82c73c993baa83f426aa01
tomasvio <tomasv.icse+git@gmail.com>
2016-08-14 14:51:51 +0200

    * @class AppSettings  * @version 0.1

---
+++ app/providers/app-settings/app-settings.ts
@@ -0,0 +1,16 @@
+import { Injectable } from '@angular/core';
+
+/**
+ *
+ *
+ * @export
+ * @class AppSettings
+ * @version 0.1
+ */
+@Injectable()
+export class AppSettings {
+
+  constructor() {}
+
+}
+
